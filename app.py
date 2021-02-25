f0b9f89e77d5884471cb9a89e3c8bcb1397bdb32
rpraka <hulprak20@gmail.com>
2021-02-24 22:52:33 -0500

    db2

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ cursor.execute(""" CREATE TABLE IF NOT EXISTS light_meta (
     );""")
 
 conn.commit()
-cursor.close()
-conn.close()
+
+
 print("table made")
 
 
@@ -24,33 +24,29 @@ print("table made")
 @app.route('/getmsg/', methods=['GET'])
 def respond():
     # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+    name = request.args.get("new_status", None)
 
-    # For debugging
-    print(f"got name {name}")
+    print(f"got new_status {name}")
+    
+    cursor.execute("SELECT * FROM light_meta;")
+
+    current_status = cursor.fetchone()[1]
 
     response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+   
+    response["message"] = f"Got new status {name}, current: {current_status}!!"
 
     # Return the response in json format
     return jsonify(response)
 
 
-    # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "<h1>lights_api</h1>"
 
+#allow close on worker termination
+# cursor.close()
+# conn.close()
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    # app.run(port=5000, workers=2, debug=True)
-    app.run(host='0.0.0.0', port=port)
+
+    app.run(port=5000, workers=2, debug=True)
