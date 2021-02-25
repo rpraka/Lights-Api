ef188792ee0ff405d6946a1d694d18f904523e67
rpraka <hulprak20@gmail.com>
2021-02-24 23:23:25 -0500

    db213

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ cursor.execute(""" CREATE TABLE IF NOT EXISTS light_meta (
     """)
 
 conn.commit()
-cursor.execute(""" INSERT INTO light_meta (id, state) VALUES (1,0);""")
+cursor.execute(""" INSERT INTO light_meta (id, state) VALUES (1,0) ;""")
 conn.commit()
 
 print("table made")
