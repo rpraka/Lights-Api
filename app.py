0bcb556add6bb148acbea2a59b4c39f45549454f
rpraka <hulprak20@gmail.com>
2021-02-24 22:27:40 -0500

    db2

--- app.py
+++ app.py
@@ -2,46 +2,46 @@ from flask import Flask, request, jsonify
 import os
 import psycopg2
 
-# DATABASE_URL = os.environ['DATABASE_URL']
-# 
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-# cursor = conn.cursor()
-# 
-# cursor.execute(""" CREATE TABLE IF NOT EXISTS light_meta (
-    # id SERIAL,
-    # state integer NOT NULL DEFAULT 0
-    # );""")
-# print("table made")
-# app = Flask(__name__)
+DATABASE_URL = os.environ['DATABASE_URL']
 
-# @app.route('/getmsg/', methods=['GET'])
-# def respond():
-#     # Retrieve the name from url parameter
-#     name = request.args.get("name", None)
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cursor = conn.cursor()
 
-#     # For debugging
-#     print(f"got name {name}")
+cursor.execute(""" CREATE TABLE IF NOT EXISTS light_meta (
+    id SERIAL,
+    state integer NOT NULL DEFAULT 0
+    );""")
+print("table made")
+app = Flask(__name__)
 
-#     response = {}
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
 
-#     # Check if user sent a name at all
-#     if not name:
-#         response["ERROR"] = "no name found, please send a name."
-#     # Check if the user entered a number not a name
-#     elif str(name).isdigit():
-#         response["ERROR"] = "name can't be numeric."
-#     # Now the user entered a valid name
-#     else:
-#         response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+    # For debugging
+    print(f"got name {name}")
 
-#     # Return the response in json format
-#     return jsonify(response)
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
 
 
-#     # A welcome message to test our server
-# @app.route('/')
-# def index():
-#     return "<h1>Welcome to our server !!</h1>"
+    # A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
