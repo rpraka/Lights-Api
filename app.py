599ee089227443696ec12e761d6edc17edccb24a
rpraka <hulprak20@gmail.com>
2021-02-24 22:22:49 -0500

    db2

--- app.py
+++ app.py
@@ -11,38 +11,38 @@ import psycopg2
     # id SERIAL,
     # state integer NOT NULL DEFAULT 0
     # );""")
-print("table made")
-app = Flask(__name__)
+# print("table made")
+# app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+# @app.route('/getmsg/', methods=['GET'])
+# def respond():
+#     # Retrieve the name from url parameter
+#     name = request.args.get("name", None)
 
-    # For debugging
-    print(f"got name {name}")
+#     # For debugging
+#     print(f"got name {name}")
 
-    response = {}
+#     response = {}
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+#     # Check if user sent a name at all
+#     if not name:
+#         response["ERROR"] = "no name found, please send a name."
+#     # Check if the user entered a number not a name
+#     elif str(name).isdigit():
+#         response["ERROR"] = "name can't be numeric."
+#     # Now the user entered a valid name
+#     else:
+#         response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
 
-    # Return the response in json format
-    return jsonify(response)
+#     # Return the response in json format
+#     return jsonify(response)
 
 
-    # A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+#     # A welcome message to test our server
+# @app.route('/')
+# def index():
+#     return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(port=5000, workers=1 + (1 * 2))
+    app.run(port=5000, workers=2)
