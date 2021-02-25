088180eb349ed6d0e1bbc87fa8fb9db516d9fb63
rpraka <hulprak20@gmail.com>
2021-02-24 22:19:35 -0500

    db2

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask, request, jsonify
 import os
 import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-cursor = conn.cursor()
-
-cursor.execute(""" CREATE TABLE IF NOT EXISTS light_meta (
-    id SERIAL,
-    state integer NOT NULL DEFAULT 0
-    );""")
+# DATABASE_URL = os.environ['DATABASE_URL']
+# 
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# cursor = conn.cursor()
+# 
+# cursor.execute(""" CREATE TABLE IF NOT EXISTS light_meta (
+    # id SERIAL,
+    # state integer NOT NULL DEFAULT 0
+    # );""")
 print("table made")
 app = Flask(__name__)
 
